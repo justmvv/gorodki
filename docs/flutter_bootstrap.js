1d3a821f9a53c4f036422c1c265b5bb4c4f69471
mrX <just.mvv@gmail.com>
2026-07-24 17:36:47 +0300

    New gameplay

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2431313216" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2714732909" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
